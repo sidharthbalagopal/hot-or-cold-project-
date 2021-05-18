65571123b3fe9d7939528bf318621c23147d27ba
sidharthbalagopal <84223376+sidharthbalagopal@users.noreply.github.com>
2021-05-18 13:35:58 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let temperature = 0
+input.onButtonPressed(Button.A, function () {
+    // if temperature less than 18 show string c and +1
+    if (true) {
+        temperature = 100
+        basic.showString("" + (temperature))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    // if temperature more than 18 show string h and -1
+    if (true) {
+        temperature = 0
+        basic.showString("" + (temperature))
+    }
+})
